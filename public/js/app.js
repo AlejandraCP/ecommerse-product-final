904f7d1a6f6d82cb78f1d50b3a55cedfe8db666e
Rocciescobar <roccisen.88@gmail.com>
2018-03-03 08:10:21 -0500

    Añade estilos mobile y desktop

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,19 @@
 const cardGroup = $('.card-group-js');
 let output = '';
-console.log(cardGroup);
 
 page('/nike', getApi);
 page('/umbro', getApi);
-page("/addidas", getApi);
-page("/reebok", getApi);
-page("/just do it", getApi);
-page("/puma", getApi);
-page("/fila", getApi);
-page("/joma", getApi);
+page('/addidas', getApi);
+page('/reebok', getApi);
+page('/just do it', getApi);
+page('/puma', getApi);
+page('/fila', getApi);
+page('/joma', getApi);
 page();
 
-function getApi(e) {  
-  let x = e.path;
-  var name = x.substr(1);
+function getApi(event) {  
+  let brand = event.path;  
+  let name = brand.substr(1);
 
   let url = `https://api.mercadolibre.com/sites/MPE/search?condition=new&q=${name}`;
 
@@ -29,17 +28,20 @@ function getApi(e) {
 
   const getApi = array => {
     let results = array.results;
+    console.log(results);    
 
     results.forEach(element => {
       output += `
-      <div class="card col-12 col-sm-4 m-2">
-        <img class="card-img-top" src="${element.thumbnail}" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">${element.title}</h5>
-          <p class="card-text"></p>
-        </div>
-        <div class="card-footer">
-          <input class="car btn btn-primary" type="button" value="AÑADIR AL CARRITO" price="${element.price}" title="${element.listing_type_id}" />
+      <div class="col-12 col-sm-12 col-md-6 col-lg-4 my-2">
+        <div class="card">
+          <img class="card-img-top" src="${element.thumbnail}" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">${element.title}</h5>
+            <p class="card-text">S/. ${element.price}</p>
+          </div>
+          <div class="card-footer text-center">
+            <input class="btn btn-info" type="button" value="AÑADIR AL CARRITO" price="${element.price}" title="${element.listing_type_id}" />
+          </div>
         </div>
       </div>
       `;
@@ -49,4 +51,3 @@ function getApi(e) {
 
   let output = '';
 };
-
